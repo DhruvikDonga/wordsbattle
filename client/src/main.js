8e3cd9aa481d4678b46ed10efce12ce1d2cf89dc
unknown <dongadhruvik@gmail.com>
2023-05-20 11:06:21 +0530

    WB-7: initial commit

--- client/src/main.js
+++ client/src/main.js
@@ -4,6 +4,20 @@ import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
 import router from './router'
 import { store } from "./store/index.js";
+const VueAnalytics = require('vue-analytics')
+
 loadFonts()
 
-createApp(App).use(router).use(vuetify).use(store).mount('#app')
+const app = createApp(App).
+    use(router).
+    use(vuetify).
+    use(store)
+
+app.use(VueAnalytics,{
+    
+    id: 'G-678VSVZR56',
+    // If you're using vue-router, pass the router instance here.
+    router,
+})
+
+app.mount('#app')
